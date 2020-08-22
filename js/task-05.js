2033ef62a9e2591f60cf63efd1620ab891770864
Viktoriia Strelnikova <v.strelnikova91@gmail.com>
2020-08-22 22:44:20 +0300

    добавляет решение задач модуля 2

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+function checkForSpam(str) {
+  let lowerStr = str.toLowerCase();
+  return lowerStr.includes('spam') || lowerStr.includes('sale');
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
